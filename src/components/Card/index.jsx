5c13b632876d1f7fe8706c6f8b374829bab20aa5
galimjan <beketovgalimjan@gmail.com>
2022-11-19 05:48:09 +0500

    form

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,10 +5,10 @@ import Grid from "@mui/material/Grid";
 function Card({ left, right, element, ...props }) {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={6}>
+      <Grid item xs={12} lg={6} sm={12}>
         {left}
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} lg={6} sm={12}>
         {right}
       </Grid>
     </Grid>
--- src/views/Form/index.jsx
+++ src/views/Form/index.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
+import { FileUploader } from "react-drag-drop-files";
+import "./style.css";
+import Button from "@mui/material/Button";
+import { useForm } from "react-hook-form";
+
+const fileTypes = ["JPG", "PNG", "GIF"];
 
 function TForm() {
-  return <Container maxWidth="lg">TForm</Container>;
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <Container maxWidth="lg">
+      <form onSubmit={handleSubmit(onSubmit)} className="upload-image">
+        <FileUploader
+          handleChange={handleChange}
+          name="file"
+          types={fileTypes}
+          classes="drag-drop-files"
+        />
+        <Button className="upload" type="submit" variant="outlined">
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
 }
 
 export default TForm;
